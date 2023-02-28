03f7baea500d52c5c76ad85fec525174330ae8e9
sarahasan17 <sarahasan.rnc@gmail.com>
2023-02-28 19:17:15 +0530

    render bug-2

--- utils/forecast.js
+++ utils/forecast.js
@@ -8,7 +8,7 @@ const forecast= (latitude,longitude,callback)=>{
             callback('Unable to find location',undefined)
            }
            else{
-            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature)
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' temperature outside.')
            }
     })
 }
